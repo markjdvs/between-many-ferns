98bd510acc7f08ab7e0def6ca8641e52df91e12d
mrkjdvs <mjd814@gmail.com>
2017-02-15 12:29:43 +0000

    list elements dynamically created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
 console.log('JS Loaded & Proud');
+
+$(() => {
+
+  const n = 5;
+  const nSq = Math.pow(n, 2);
+  const $board = $('ul');
+  const $tiles = $('li');
+  const tilePics = ['yellow', 'blue', 'green', 'red'];
+
+  function tilePicGenerator (i) {
+    console.log(i);
+    const randPicNum = Math.floor(Math.random()*tilePics.length);
+    console.log('within function');
+    const randColor = `${tilePics[randPicNum]}`;
+    console.log('randPicNum from function');
+    $tiles.eq(i).attr('class', randColor);
+  }
+
+  function genBoard () {
+    const tile = '<li></li>';
+
+    for (let i = 0; i<nSq; i++) {
+      $board.append(tile);
+    }
+  }
+
+  // function verticalStreaker () {
+  //
+  // }
+  //
+  // function horizontalStreaker () {
+  //
+  // }
+  //
+  // function boardHasStreaks () {
+  //
+  // }
+
+  genBoard();
+
+});
